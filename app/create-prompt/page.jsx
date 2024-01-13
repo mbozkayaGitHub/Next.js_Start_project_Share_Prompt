a69221d1226ec1a99e41a8600f875ccf8b61884f
mbozkayaGitHub <mbozkaya27@gmail.com>
2024-01-13 21:35:42 +0300

    cretaingFormPage

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Form from "@components/Form";
+import CreatePrompt from "./page";
+const CreatePrompt = () => {
+  const [post, setPost] = useState({
+    propt: "",
+    tag: "",
+  });
+  const [submitting, setSubmitting] = useState(false);
+  const createPrompt = async (e) => {};
   return (
-    <div>page</div>
-  )
-}
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
 
-export default page
+export default CreatePrompt;
