4b4314fc739d8f92d6dc2160b950afddbaf30435
mbozkayaGitHub <mbozkaya27@gmail.com>
2024-01-13 21:19:06 +0300

    SignOut

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,8 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 //import {authConfig} from "@/lib/auth"
-import { connectToDB } from '@utils/database';
-import User from '@models/user';
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 // console.log({
 //   clientId: process.env.GOOGLE_ID,
@@ -10,26 +10,26 @@ import User from '@models/user';
 // })
 
 const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 
-
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID  ,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      })
-    ],  async session({ session }) {
-         // store the user id from MongoDB to session
+  callback: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
       const sessionUser = await User.findOne({ email: session.user.email });
       session.user.id = sessionUser._id.toString();
-
       return session;
-      },
-      async signIn({ profile }) {
-try {
-  await connectToDB();
-   // check if user already exists
-   const userExists = await User.findOne({ email: profile.email });
-   
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
         // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
@@ -38,13 +38,13 @@ try {
             image: profile.picture,
           });
         }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+});
 
-        return true
-} catch (error) {
-  console.log(error);
-  return false 
-}
-
-      }})
-
-      export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
