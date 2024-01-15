9aef7e54f10e7c3d90553ac747983689c1017fa5
mbozkayaGitHub <mbozkaya27@gmail.com>
2024-01-15 22:05:15 +0300

    update

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,13 +5,18 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 import CreatePrompt from "./page";
+
+
 const CreatePrompt = () => {
   const [post, setPost] = useState({
-    propt: "",
+    prompt: "",
     tag: "",
   });
+  const createPrompt = async (e) => {
+
+  }
   const [submitting, setSubmitting] = useState(false);
-  const createPrompt = async (e) => {};
+  
   return (
     <Form
       type="Create"
