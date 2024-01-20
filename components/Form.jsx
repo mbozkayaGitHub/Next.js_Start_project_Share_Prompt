530bcf2f8d71239be9eb6bbd124a29b532720e0c
mbozkayaGitHub <mbozkaya27@gmail.com>
2024-01-20 23:46:58 +0300

    handleSubmit

--- components/Form.jsx
+++ components/Form.jsx
@@ -13,6 +13,18 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
         {type} and share amazing prompts with the world, and let your
         imagination run wild with any AI-powered platform
       </p>
+        
+      <form
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
+      >
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Your AI Prompt
+          </span>
+          </label>
+          </form>
+
       </section>
     </div>
   );
