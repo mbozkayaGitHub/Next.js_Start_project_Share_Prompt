33bde186e63cc34416f7962b565edc5ba94bc5e5
mbozkayaGitHub <mbozkaya27@gmail.com>
2024-02-12 20:20:19 +0300

    update

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -8,14 +8,14 @@ import Form from "@components/Form";
 const CreatePrompt = () => {
   const router = useRouter();
   const { data: session } = useSession();
-  const [submitting, setSubmitting] = useState(false);
+  const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
   const createPrompt = async (e) => {
     e.preventDefault();
-    setSubmitting(true);
+    setIsSubmitting(true);
 
     try {
       const response = await fetch("/api/prompt/new", {
@@ -33,7 +33,7 @@ const CreatePrompt = () => {
     } catch (error) {
       console.log(error);
     } finally {
-      setSubmitting(false);
+      setIsSubmitting(false);
     }
   };
 
